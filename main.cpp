eeff4fffcca6f8bea239f4cc094ef6c8b86c936d
Alex Kireev <alexkireev15@gmail.com>
2021-08-20 22:23:57 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -72,59 +72,5 @@ int main()
 
         in.close();
     }
-
-    
-    /*{
-        std::array<double, 9> t1, t2;
-        for (size_t idx = 0; idx < 9; ++idx)
-        {
-            std::cin >> t1[idx];
-        }
-        for (size_t idx = 0; idx < 9; ++idx)
-        {
-            std::cin >> t2[idx];
-        }
-
-        bool answer = KireevA::HasIntersection(t1, t2);
-
-        std::cout << std::endl;
-        for (size_t idx = 0; idx < 3; ++idx)
-        {
-            for (size_t jdx = 0; jdx < 3; ++jdx)
-            {
-                std::cout << t1[idx * 3 + jdx] << "; ";
-            }
-            std::cout << std::endl;
-        }
-        std::cout << std::endl;
-        for (size_t idx = 0; idx < 3; ++idx)
-        {
-            for (size_t jdx = 0; jdx < 3; ++jdx)
-            {
-                std::cout << t2[idx * 3 + jdx] << "; ";
-            }
-            std::cout << std::endl;
-        }
-
-        if (answer)
-        {
-            std::cout << "Has Intersection" << std::endl;
-        }
-        else
-        {
-            std::cout << "No Intersection" << std::endl;
-        }
-    }*/
-    
-    /*KireevA::Segment s(KireevA::Point(4., 4., 0.), KireevA::Point(2., 2., 0.));
-    KireevA::Segment s2(KireevA::Point(4., 2., 0.), KireevA::Point(2., 4., 0.));
-    auto res = s.Intersect(s2);
-
-    for (auto it = res.begin(); it != res.end(); ++it)
-    {
-        std::cout << it->GetX() << " " << it->GetY() << " " << it->GetZ() << std::endl;
-    }*/
-
-
     return 0;
 }
